7df89e39badcbe088fc3ee6c061fb0b7650b2e7f
Grant Carthew <grant@carthew.net>
2019-08-02 16:40:10 +1000

    Added mustache-express

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mustacheExpress = require('mustache-express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello Demo2 App!'))
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.get('/', (req, res) => {
+  res.render('index', { data: process.env.data })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
